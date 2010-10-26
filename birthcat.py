33dec7a68686e8594e6208ca1841cc7f79421b9c
silvonen <silvonen@6be55e7f-d3e8-44be-ad89-eb7ff8f327b7>
2010-10-26 17:39:57 +0000

    Ensimmäiset skriptit:
    - birthcat.py: henkilöiden luokittelu syntymä- ja kuolinvuosiluokkiin (kehitteillä)
    - sciname.py: ohjausten lisääminen tieteellisistä nimistä eliöartikkeleihin.
    
    git-svn-id: https://svn.code.sf.net/p/pywikipedia-fi/code/trunk@2 6be55e7f-d3e8-44be-ad89-eb7ff8f327b7

---
+++ birthcat.py
@@ -0,0 +1,264 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8  -*-
+"""
+Categorize people according to their year of birth and death.
+
+The following parameters are supported:
+
+&params;
+
+-dry              If given, doesn't do any real changes, but only shows
+                  what would have been changed.
+
+All other parameters will be regarded as part of the title of a single page,
+and the bot will only work on that single page.
+"""
+__version__ = '$Id$'
+import wikipedia as pywikibot
+import catlib, pagegenerators, re
+
+# This is required for the text that is shown when you run this script
+# with the parameter -help.
+docuReplacements = {
+    '&params;': pagegenerators.parameterHelp
+}
+
+class BirthCatBot:
+    # Edit summary message that should be used.
+    # NOTE: Put a good description here, and add translations, if possible!
+    msg = {
+        'fi': u'Botti lisäsi: %s',
+    }
+
+    msgNewCat = {
+        'fi': u'Botti loi uuden luokan',
+    }
+
+    dateFormat = {
+        'fi': ur'(?:\[*\d+\.\s+\S+kuuta\]*,?\s+)?\[*(\d+)\]*',
+        }
+    
+    birthDeathCats = {
+        'fi': {
+            u'Vuonna %s syntyneet' : {
+                'templates' :{
+                    u'Syntymäaika ja ikä': 3,
+                    u'Syntymä- ja kuolinaika': 3,
+                    u'Kuolinaika ja ikä': 3,
+                    },
+                're': [
+                    re.compile(ur'syntynyt\s*=\s*' + dateFormat['fi']),
+                    re.compile(ur"'''[^)]*(?:\bs\.\s+|,\s+|\()" + dateFormat['fi']),
+                    ],
+                'newcat': lambda yr: u'{{Syntymävuosiluokka|%s|%s}}' % (yr[:-1], yr[-1])
+                },
+            u'Vuonna %s kuolleet' : {
+                'templates' :{
+                    u'Syntymä- ja kuolinaika': 6,
+                    u'Kuolinaika ja ikä': 6,
+                    },
+                're': [
+                    re.compile(ur'kuollut\s*=\s*' + dateFormat['fi']),
+                    re.compile(ur"'''[^)]*(?:\bk\.\s+|(?:[–—-]|&ndash;)\s*)" + dateFormat['fi']),
+                    ],
+                'newcat': lambda yr: u'{{Kuolinvuosiluokka|%s|%s}}' % (yr[:-1], yr[-1])
+                }
+            }
+        }
+    
+    def __init__(self, generator, dry):
+        """
+        Constructor. Parameters:
+            * generator - The page generator that determines on which pages
+                          to work on.
+            * dry       - If True, doesn't do any real changes, but only shows
+                          what would have been changed.
+        """
+        self.generator = generator
+        self.dry = dry
+        self.lang = pywikibot.getSite().lang
+        
+        # Get the correct localized parameters
+        self.site = pywikibot.getSite()
+        self.summary = pywikibot.translate(self.site, self.msg)
+        self.summaryNewCat = pywikibot.translate(self.site, self.msgNewCat)
+        self.bdCats = pywikibot.translate(self.site, self.birthDeathCats)
+
+        self.defaultSortMatcher = None        
+        defaultSortLabels = self.site.getmagicwords('defaultsort')
+        if defaultSortLabels:
+            self.defaultSortMatcher = re.compile('\{\{(' + '|'.join(defaultSortLabels) + ')')
+        
+    def run(self):
+        try:
+            for page in self.generator:
+                self.treat(page)
+        except KeyboardInterrupt:
+            pywikibot.output('\nQuitting program...')
+
+    def treat(self, page):
+        """
+        Adds the page to the appropriate birth and death year categories.
+        """
+        pywikibot.output(u"Processing page %s..." % page.title(asLink=True))
+        text = self.load(page)
+        if not text:
+            return
+        
+        cats = page.categories()
+        addCats = []
+        newCatContent = {}
+        foundTemplates = False
+        for tmpl, params in page.templatesWithParams():
+            numParams = filter(lambda s: s.isdigit(), params) # Filter out birthplace fields
+            for bdCat, bdCatInfo in self.bdCats.iteritems():
+                if tmpl in bdCatInfo['templates']:
+                    pywikibot.output(u'Found template {{%s|%s}}' % (tmpl, u'|'.join(params)))
+                    foundTemplates = True
+                    try:
+                        year = numParams[bdCatInfo['templates'][tmpl] - 1].strip()
+                    except IndexError:
+                        continue
+                    if not self.defaultSortMatcher or not cats or self.defaultSortMatcher.search(text):
+                        sortKey = None
+                    else:
+                        sortKey = cats[0].sortKey
+                    newCat = catlib.Category(self.site, bdCat % year, sortKey=sortKey)
+                    if newCat in cats or newCat in addCats:
+                        pywikibot.output(u"%s is already in %s." % (page.title(), newCat.title()))
+                    else:
+                        addCats.append(newCat)
+                        newCatContent[newCat.title()] = bdCatInfo['newcat'](year)
+
+        if not addCats and not foundTemplates:
+            for bdCat, bdCatInfo in self.bdCats.iteritems():
+                for reMatcher in bdCatInfo['re']:
+                    match = reMatcher.search(text)
+                    if match:
+                        year = match.group(1)
+                        pywikibot.output(u'Found %s in %s' % (year, match.group(0)))
+                        if not self.defaultSortMatcher or not cats or self.defaultSortMatcher.search(text):
+                            sortKey = None
+                        else:
+                            sortKey = cats[0].sortKey
+                        newCat = catlib.Category(self.site, bdCat % year, sortKey=sortKey)
+                        if newCat in cats or newCat in addCats:
+                            pywikibot.output(u"%s is already in %s." % (page.title(), newCat.title()))
+                        else:
+                            addCats.append(newCat)
+                            newCatContent[newCat.title()] = bdCatInfo['newcat'](year)
+
+        if addCats:
+            cats.extend(addCats)
+            text = pywikibot.replaceCategoryLinks(text, cats)
+##            pywikibot.output(text) # Debugging...
+            summary = self.summary % ', '.join(map(lambda c: c.title(asLink=True), addCats))
+            if not self.save(text, page, summary):
+                pywikibot.output(u'Page %s not saved.' % page.title(asLink=True))
+                return
+            for cat in addCats:
+                if not cat.exists():
+                    pywikibot.output(u'%s should be created.' % (cat.title()))
+                    if not self.save(newCatContent[cat.title()], cat, self.summaryNewCat, minorEdit=False):
+                        pywikibot.output(u'%s not created.' % cat.title(asLink=True))
+
+    def load(self, page):
+        """
+        Loads the given page, does some changes, and saves it.
+        """
+        try:
+            # Load the page
+            text = page.get()
+        except pywikibot.NoPage:
+            pywikibot.output(u"Page %s does not exist; skipping."
+                             % page.title(asLink=True))
+        except pywikibot.IsRedirectPage:
+            pywikibot.output(u"Page %s is a redirect; skipping."
+                             % page.title(asLink=True))
+        else:
+            return text
+        return None
+
+    def save(self, text, page, comment, minorEdit=True, botflag=True):
+        # only save if something was changed
+        try:
+            oldText = page.get()
+        except pywikibot.NoPage:
+            oldText = ""
+        if text != oldText:
+            # Show the title of the page we're working on.
+            # Highlight the title in purple.
+            pywikibot.output(u"\n\n>>> \03{lightpurple}%s\03{default} <<<"
+                             % page.title())
+            # show what was changed
+            pywikibot.showDiff(oldText, text)
+            pywikibot.output(u'Comment: %s' %comment)
+            if not self.dry:
+                choice = pywikibot.inputChoice(
+                    u'Do you want to accept these changes?',
+                    ['Yes', 'No'], ['y', 'N'], 'N')
+                if choice == 'y':
+                    try:
+                        # Save the page
+                        page.put(text, comment=comment,
+                                 minorEdit=minorEdit, botflag=botflag)
+                    except pywikibot.LockedPage:
+                        pywikibot.output(u"Page %s is locked; skipping."
+                                         % page.title(asLink=True))
+                    except pywikibot.EditConflict:
+                        pywikibot.output(
+                            u'Skipping %s because of edit conflict'
+                            % (page.title()))
+                    except pywikibot.SpamfilterError, error:
+                        pywikibot.output(u'Cannot change %s because of spam blacklist entry %s'
+                            % (page.title(), error.url))
+                    else:
+                        return True
+        return False
+
+def main():
+    # This factory is responsible for processing command line arguments
+    # that are also used by other scripts and that determine on which pages
+    # to work on.
+    genFactory = pagegenerators.GeneratorFactory()
+    # The generator gives the pages that should be worked upon.
+    gen = None
+    # This temporary array is used to read the page title if one single
+    # page to work on is specified by the arguments.
+    pageTitleParts = []
+    # If dry is True, doesn't do any real changes, but only show
+    # what would have been changed.
+    dry = False
+
+    # Parse command line arguments
+    for arg in pywikibot.handleArgs():
+        if arg.startswith("-dry"):
+            dry = True
+        else:
+            # check if a standard argument like
+            # -start:XYZ or -ref:Asdf was given.
+            if not genFactory.handleArg(arg):
+                pageTitleParts.append(arg)
+
+    if pageTitleParts != []:
+        # We will only work on a single page.
+        pageTitle = ' '.join(pageTitleParts)
+        page = pywikibot.Page(pywikibot.getSite(), pageTitle)
+        gen = iter([page])
+
+    if not gen:
+        gen = genFactory.getCombinedGenerator()
+    if gen:
+        # The preloading generator is responsible for downloading multiple
+        # pages from the wiki simultaneously.
+        gen = pagegenerators.PreloadingGenerator(gen)
+        bot = BirthCatBot(gen, dry)
+        bot.run()
+    else:
+        pywikibot.showHelp()
+
+if __name__ == "__main__":
+    try:
+        main()
+    finally:
+        pywikibot.stopme()
---
+++ sciname.py
@@ -0,0 +1,186 @@
+#!/usr/bin/python
+# -*- coding: utf-8  -*-
+"""
+Add redirects from scientific names to articles on living organisms.
+
+The following parameters are supported:
+
+&params;
+
+-all              Work on all pages that use a taxobox template
+
+-always           Don't prompt you for each redirect page to be added
+
+-dry              If given, doesn't do any real changes, but only shows
+                  what would have been changed.
+
+All other parameters will be regarded as part of the title of a single page,
+and the bot will only work on that single page.
+"""
+__version__ = '$Id$'
+import wikipedia as pywikibot
+import pagegenerators, pickle, re
+
+# This is required for the text that is shown when you run this script
+# with the parameter -help.
+docuReplacements = {
+    '&params;': pagegenerators.parameterHelp
+}
+
+class SciNameBot:
+    # Edit summary message that should be used.
+    # NOTE: Put a good description here, and add translations, if possible!
+    msg = {
+        'en': u'Robot: Adding a scientific name for %s',
+        'fi': u'Botti lisäsi eliön %s tieteellisen nimen',
+    }
+
+    taxoboxTemplates = {
+        'fi': [ u'Taksonomia/eläimet', u'Taksonomia/kasvit', u'Taksonomia/sienet' ]
+    }
+
+    sciNameParameters = {
+        'fi': [ u'kaksiosainen', u'kolmiosainen' ]
+    }
+
+    cacheFilename = u'cache/sciname'
+    
+    def __init__(self, generator, dry, always):
+        """
+        Constructor. Parameters:
+            * generator - The page generator that determines on which pages
+                          to work on.
+            * dry       - If True, doesn't do any real changes, but only shows
+                          what would have been changed.
+            * always    - If True, don't prompt for each redirect page.
+        """
+        self.generator = generator
+        self.dry = dry
+        self.always = always
+        self.lang = pywikibot.getSite().lang
+        
+        # Set the edit summary message
+        self.summary = pywikibot.translate(pywikibot.getSite(), self.msg)
+        self.templates = pywikibot.translate(pywikibot.getSite(), self.taxoboxTemplates)
+        self.templateParameters = pywikibot.translate(pywikibot.getSite(), self.sciNameParameters)
+
+        # Initialize the cache
+        try:
+            self.cache = pickle.load(file(self.cacheFilename, 'rb'))
+        except:
+            self.cache = {}
+        if not self.lang in self.cache:
+            self.cache[self.lang] = {}
+
+    def getSciName(self, params):
+        for param in params:
+            for sciNameParam in self.templateParameters:
+                m = re.search(u'^\s*%s\s*=([^<{]*)' % sciNameParam, param)
+                if m:
+                    sciname = m.group(1).strip()
+                    if sciname:
+                        return sciname
+        return None
+        
+    def run(self):
+        try:
+            for page in self.generator:
+                self.treat(page)
+        except KeyboardInterrupt:
+            pywikibot.output('\nQuitting program...')
+        pickle.dump(self.cache, file(self.cacheFilename, 'wb'))
+
+    def treat(self, page):
+        """
+        Adds a redirect from scientific name to page if it does not exist.
+        """
+        pywikibot.output(u"Processing page %s..." % page.aslink())
+        for tmpl, params in page.templatesWithParams():
+            if tmpl in self.templates:
+                sciName = self.getSciName(params)
+                if not sciName:
+                    pywikibot.output(u"Scientific name not found; skipping.")
+                elif sciName.find(u"''") != -1:
+                    pywikibot.output(u"Text formatting in scientific name; skipping.")
+                elif sciName in self.cache[self.lang]:
+                    pywikibot.output(u'"%s" found in cache; skipping.' % sciName)
+                else:
+                    redirPage = pywikibot.Page(pywikibot.getSite(), sciName)
+                    if redirPage.exists():
+                        pywikibot.output(u"Page %s exists; skipping." % redirPage.aslink())
+                        self.cache[self.lang][sciName] = page.title()
+                    else:
+                        pywikibot.output(u"Creating redirect page %s." % redirPage.aslink())
+                        text = u'#%s %s' % (pywikibot.getSite().redirect(True), page.aslink())
+                        if not self.dry:
+                            if self.always:
+                                choice = 'y'
+                            else:
+                                choice = pywikibot.inputChoice(u'Do you want to create this page?', ['Yes', 'No'], ['y', 'N'], 'N')
+                            if choice == 'y':
+                                try:
+                                    # Save the page
+                                    redirPage.put(text, comment=self.summary % page.aslink())
+                                except pywikibot.LockedPage:
+                                    pywikibot.output(u"Page %s is locked; skipping." % redirPage.aslink())
+                                except pywikibot.EditConflict:
+                                    pywikibot.output(u'Skipping %s because of edit conflict' % (redirPage.title()))
+                                except pywikibot.SpamfilterError, error:
+                                    pywikibot.output(u'Cannot change %s because of spam blacklist entry %s' % (redirPage.title(), error.url))
+                                else:
+                                    self.cache[self.lang][sciName] = page.title()
+
+def main():
+    # This factory is responsible for processing command line arguments
+    # that are also used by other scripts and that determine on which pages
+    # to work on.
+    genFactory = pagegenerators.GeneratorFactory()
+    # The generator gives the pages that should be worked upon.
+    gen = None
+    # This temporary array is used to read the page title if one single
+    # page to work on is specified by the arguments.
+    pageTitleParts = []
+    # If dry is True, doesn't do any real changes, but only show
+    # what would have been changed.
+    dry = False
+    # If always is True, don't confirm changes.
+    always = False
+
+    # Parse command line arguments
+    for arg in pywikibot.handleArgs():
+        if arg.startswith("-dry"):
+            dry = True
+        elif arg.startswith("-always"):
+            always = True
+        elif arg.startswith("-all"):
+            genFactory.handleArg('-namespace:0')
+            for tmpl in pywikibot.translate(pywikibot.getSite(), SciNameBot.taxoboxTemplates):
+                genFactory.handleArg('-transcludes:%s' % tmpl)
+        else:
+            # check if a standard argument like
+            # -start:XYZ or -ref:Asdf was given.
+            if not genFactory.handleArg(arg):
+                pageTitleParts.append(arg)
+
+    if pageTitleParts != []:
+        # We will only work on a single page.
+        pageTitle = ' '.join(pageTitleParts)
+        page = pywikibot.Page(pywikibot.getSite(), pageTitle)
+        gen = iter([page])
+
+    if not gen:
+        gen = genFactory.getCombinedGenerator()
+    if gen:
+        # The preloading generator is responsible for downloading multiple
+        # pages from the wiki simultaneously.
+        gen = pagegenerators.PreloadingGenerator(gen)
+        bot = SciNameBot(gen, dry, always)
+        bot.run()
+    else:
+        pywikibot.showHelp()
+
+if __name__ == "__main__":
+    try:
+        main()
+    finally:
+        pywikibot.stopme()
